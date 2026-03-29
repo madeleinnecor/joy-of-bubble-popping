018bb58d5d905ece6d4b58cd61698736a33e4a99
madeleinnecor <140936184+madeleinnecor@users.noreply.github.com>
2026-03-29 16:57:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    bubble.tossBubble()
+    bubble.load_bubble()
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    bubble.tilt_angle(bubble.Choice.Right)
+})
+scene.onHitWall(SpriteKind.Bubble, function (sprite, location) {
+    bubble.stick_to_wall(sprite, location)
+})
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    bubble.tilt_angle(bubble.Choice.Left)
+})
+bubble.createBoard()
+music.play(music.createSong(assets.song`music1`), music.PlaybackMode.LoopingInBackground)
+bubble.load_bubble()
+let mySprite = sprites.create(io2_images.joy, SpriteKind.Player)
+mySprite.top = 102
